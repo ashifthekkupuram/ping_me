88a36cb39e9f2ae940d5e7d68e07cda8f08012bc
Ashif Thekkupuram <ashifhussain4548@gamil.com>
2024-09-24 17:18:19 +0530

    login and register page design almost done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,51 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Box, styled, TextField, Typography, Button } from '@mui/material'
+import { Box, styled, TextField, Typography, Button, CircularProgress } from '@mui/material'
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { useNavigate } from 'react-router-dom';
+
+const MainBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 0,
+  width: '100%',
+  height: '100vh',
+  backgroundColor: theme.palette.background.default
+}))
+
+const FormBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+  padding: '50px 25px',
+  backgroundColor:  theme.palette.primary.contrastText,
+  borderRadius: '10px',
+  boxShadow: theme.shadows[3],
+  maxWidth: '500px'
+}))
+
+const CustomChatBubleIcon = styled(ChatBubbleIcon)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  fontSize: '40px',
+  alignSelf: 'center'
+}))
+
+const Title = styled(Typography)(({theme}) => ({
+  color: theme.palette.text.primary,
+  marginBottom: '8px',
+  alignSelf: 'center'
+}))
+
+const LoginButton = styled(Button)(({ theme }) => ({
+  marginTop: '10px',
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  '&.Mui-disabled': {
+  backgroundColor: theme.palette.action.disabledBackground,
+  color: theme.palette.action.disabled
+},
+}))
+
+const CustomLink = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  '&:hover':{
+    cursor: 'pointer'
+  }
+}))
 
 const Login = () => {
 
-  const MainBox = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 0,
-    width: '100%',
-    height: '100vh',
-    backgroundColor: theme.palette.background.default
-  }))
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const FormBox = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '8px',
-    padding: '50px 25px',
-    backgroundColor:  theme.palette.primary.contrastText,
-    borderRadius: '10px',
-    boxShadow: theme.shadows[3],
-    minWidth: '300px'
-  }))
+  const navigate = useNavigate()
 
-  const Title = styled(Typography)(({theme}) => ({
-    color: theme.palette.text.primary,
-    marginBottom: '8px'
-  }))
-
-  const LoginButton = styled(Button)(({ theme }) => ({
-    marginTop: '10px',
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    '&:hover': {
-      backgroundColor: theme.palette.primary.dark,
-    },
-  }))
+  const onChange = (e) => {
+    setForm((prev) => ({...prev, [e.target.id]: e.target.value}))
+  }
 
   return (
     <MainBox>
       <FormBox>
-       <Title variant='h4'>Login</Title>
-       <TextField variant='outlined' label='Email' required fullWidth />
-       <TextField type='password' variant='outlined' label='Password' required fullWidth />
-       <LoginButton loading={true} loadingIndicator="Loading…" >Login</LoginButton>
+        <CustomChatBubleIcon />
+        <Title variant='h4'>Login</Title>
+        <TextField type='email' value={form.email} variant='outlined' name='email' id='email' label='Email' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <TextField type='password' value={form.password} variant='outlined' name='password' id='password' label='Password' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <LoginButton disabled={loading || !form.email || !form.password} >{ loading ? <CircularProgress size={24} /> : 'Login'}</LoginButton>
+        <CustomLink variant='small' onClick={(e)=>navigate('/')}>Forgot password?</CustomLink>
+        <CustomLink variant='small' onClick={(e)=>navigate('/register')}>Don't have an account?</CustomLink>
       </FormBox> 
     </MainBox>
   )
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Box, styled, TextField, Typography, Button, CircularProgress } from '@mui/material'
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { useNavigate } from 'react-router-dom';
+
+const MainBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 0,
+  width: '100%',
+  height: '100vh',
+  backgroundColor: theme.palette.background.default
+}))
+
+const FormBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+  padding: '50px 25px',
+  backgroundColor:  theme.palette.primary.contrastText,
+  borderRadius: '10px',
+  boxShadow: theme.shadows[3],
+  width: '500px'
+}))
+
+const CustomChatBubleIcon = styled(ChatBubbleIcon)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  fontSize: '40px',
+  alignSelf: 'center'
+}))
+
+const Title = styled(Typography)(({theme}) => ({
+  color: theme.palette.text.primary,
+  marginBottom: '8px',
+  alignSelf: 'center'
+}))
+
+const RegisterButton = styled(Button)(({ theme }) => ({
+  marginTop: '10px',
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  '&.Mui-disabled': {
+  backgroundColor: theme.palette.action.disabledBackground,
+  color: theme.palette.action.disabled
+},
+}))
+
+const CustomLink = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  '&:hover':{
+    cursor: 'pointer'
+  }
+}))
 
 const Register = () => {
+
+  const [form, setForm] = useState({
+    firstName: '',
+    secondName: '',
+    email: '',
+    password: '',
+  })
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setForm((prev) => ({...prev, [e.target.id]: e.target.value}))
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <MainBox>
+      <FormBox>
+        <CustomChatBubleIcon />
+        <Title variant='h4'>Login</Title>
+        <TextField type='text' value={form.firstName} variant='outlined' name='firstName' id='firstName' label='First Name' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <TextField type='text' value={form.secondName} variant='outlined' name='secondName' id='secondName' label='Second Name' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <TextField type='email' value={form.email} variant='outlined' name='email' id='email' label='Email' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <TextField type='password' value={form.password} variant='outlined' name='password' id='password' label='Password' onChange={onChange} error={error} helperText={error} required fullWidth />
+        <RegisterButton disabled={loading || !form.email || !form.password || !form.firstName || !form.secondName} >{ loading ? <CircularProgress size={24} /> : 'Login'}</RegisterButton>
+        <CustomLink variant='small' onClick={(e)=>navigate('/login')}>Already have an account?</CustomLink>
+      </FormBox> 
+    </MainBox>
   )
 }
 

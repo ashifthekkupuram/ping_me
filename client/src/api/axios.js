a34f07e6d589e9c650fc5c64fb9a0d6f5108eb66
Ashif Thekkupuram <ashifhussain4548@gamil.com>
2024-09-28 03:50:50 +0530

    navbar halfway done

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_API_URL
 
-console.log(baseURL)
-
 export default axios.create({
     baseURL,
     withCredentials: true
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { AppBar, styled, Toolbar, Typography, Switch, Avatar } from '@mui/material'
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { toggleTheme } from '../redux/slices/darkModeSlice';
+
+const CustomToolBar = styled(Toolbar)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.contrastText
+}))
+
+const CustomChatBubleIcon = styled(ChatBubbleIcon)(({ theme }) => ({
+  color: theme.palette.text.primary,
+}))
+
+const Title = styled(Typography)(({ theme }) => ({
+  display: 'block',
+  color: theme.palette.text.primary,
+  fontSize: '24px',
+  fontWeight: '500',
+  marginLeft: '5px',
+  [theme.breakpoints.down('sm')]: {
+    display: 'none'
+  }
+}))
 
 const NavBar = () => {
+
+  const darkMode = useSelector((state) => state.darkMode)
+  const UserData = useSelector((state) => state.auth.UserData)
+  const dispatch = useDispatch()
+
+  const onToggle = () => {
+    dispatch(toggleTheme())
+  }
+
   return (
-    <div>
-      NavBar
-    </div>
+    <AppBar>
+      <CustomToolBar>
+        <CustomChatBubleIcon />
+        <Title variant='h1'>Ping me</Title>
+        <Switch checked={darkMode} onClick={onToggle} />
+        <Avatar alt={`${UserData.name?.firstName} ${UserData.name?.secondName}`} src={UserData?.profile} />
+      </CustomToolBar>
+    </AppBar>
   )
 }
 

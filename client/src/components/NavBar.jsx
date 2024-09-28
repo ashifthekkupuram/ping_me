9a039ca39a6cdbf897865b0fced914c2a3ef0e93
Ashif Thekkupuram <ashifhussain4548@gamil.com>
2024-09-28 17:31:31 +0530

    navbar design fully done

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { AppBar, styled, Toolbar, Typography, Switch, Avatar } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, styled, Toolbar, Typography, Switch, Avatar, Box, IconButton } from '@mui/material'
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { toggleTheme } from '../redux/slices/darkModeSlice';
 
 const CustomToolBar = styled(Toolbar)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.contrastText
+  backgroundColor: theme.palette.primary.contrastText,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+}))
+
+const CustomIconButton = styled(IconButton)(({ theme }) => ({
+  display: 'none',
+  marginRight: 2,
+  [theme.breakpoints.down('sm')]: {
+    display: 'block',
+  }
 }))
 
 const CustomChatBubleIcon = styled(ChatBubbleIcon)(({ theme }) => ({
@@ -18,13 +31,20 @@ const Title = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
   fontSize: '24px',
   fontWeight: '500',
-  marginLeft: '5px',
   [theme.breakpoints.down('sm')]: {
     display: 'none'
   }
 }))
 
+const Section = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '5px'
+}))
+
 const NavBar = () => {
+
+  const [showMenu, showMenuIcon] = useState(false)
 
   const darkMode = useSelector((state) => state.darkMode)
   const UserData = useSelector((state) => state.auth.UserData)
@@ -34,13 +54,24 @@ const NavBar = () => {
     dispatch(toggleTheme())
   }
 
+  const onMenu = () => {
+    showMenuIcon((prev) => !prev)
+  }
+
   return (
     <AppBar>
       <CustomToolBar>
-        <CustomChatBubleIcon />
-        <Title variant='h1'>Ping me</Title>
-        <Switch checked={darkMode} onClick={onToggle} />
-        <Avatar alt={`${UserData.name?.firstName} ${UserData.name?.secondName}`} src={UserData?.profile} />
+        <Section>
+          <CustomIconButton onClick={onMenu} size='small' edge='start'>
+            {showMenu ? <MenuOpenIcon /> : <MenuIcon />}
+          </CustomIconButton>
+          <CustomChatBubleIcon />
+          <Title variant='h1'>Ping me</Title>
+        </Section>
+        <Section>
+          <Switch checked={darkMode} onClick={onToggle} />
+          <Avatar alt={`${UserData.name?.firstName} ${UserData.name?.secondName}`} src={UserData?.profile} />
+        </Section>
       </CustomToolBar>
     </AppBar>
   )

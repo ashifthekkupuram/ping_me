4d050e402fed44314644368cc01a8435b681ec08
Ashif Thekkupuram <ashifhussain4548@gamil.com>
2024-11-30 16:20:50 +0530

    auto token refresh implemented

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { refresh } from '../redux/slices/authSlice'
+import { refresh, logout } from '../redux/slices/authSlice'
 
 const baseURL = import.meta.env.VITE_API_URL
 
@@ -31,14 +31,19 @@ instance.interceptors.response.use(
     async (response) => {
         return response
     },
-    (error) => {
-        let alreadyRefreshed = false
+    async (error) => {
         if(error.status === 403){
-            if(!alreadyRefreshed){
-
-            }else{
-
+            try{
+                const result = await store.dispatch(refresh())
+                if(result.payload.success){
+                    return instance.request(error.config)
+                }else{
+                    await store.dispatch(logout())
+                }
+            } catch(err) {
+                await store.dispatch(logout())
             }
+                
         }
         return Promise.reject(error)
     }
